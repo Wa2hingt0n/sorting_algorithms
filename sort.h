7e08f739738281ec5dd3b34237b3a22e7e2ac425
Washington Obungu <tobungu@gmail.com>
2022-01-12 06:59:59 -0800

    Updated header file

--- sort.h
+++ sort.h
@@ -1,8 +1,10 @@
 #ifndef SORT_H
 #define SORT_H
 
+/* Header files to include */
 #include <stddef.h>
 
+/* Doubly linked list data structure definition */
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -26,6 +28,14 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
 /* 3-quick_sort.c */
 void swap_ints(int *a, int *b);
